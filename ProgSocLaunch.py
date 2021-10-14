00bdedcc60c5f23fd4913b98748e0c8efd54738b
Rowena <Rowenalee2332@gmail.com>
2021-10-14 16:22:15 +0100

    my shape

--- ProgSocLaunch.py
+++ ProgSocLaunch.py
@@ -1,6 +1,6 @@
 import pygame
 
-import sergi
+import rowena
 
 
 SCREEN_WIDTH = 1280
@@ -17,7 +17,7 @@ def update(screen):
     # draw onto screen
     screen.fill((255, 255, 255))
 
-    sergi.draw(screen, 100, 100)
+    rowena.draw(screen, 300, 300)
 
     pygame.display.flip()
 
